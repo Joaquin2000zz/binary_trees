9adcde3589f6fdd670c4ed71e305e3c24586fea8
Santi3948 <santiagonegreyra@gmail.com>
2022-03-23 11:00:10 -0700

    update 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,8 +7,6 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *aux;
-
 	if (!node || !node->parent)
 		return (NULL);
 	if (node->parent->right && (node->parent->right == node))
