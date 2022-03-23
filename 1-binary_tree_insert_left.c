84a456130edeb17e1dae8b0628572e56e8642b3a
Santi3948 <santiagonegreyra@gmail.com>
2022-03-23 10:22:28 -0700

    update 2 binary and 1 binary

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,8 +10,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-	if (!new || !parent)
+	if (!new)
 		return (NULL);
+	if (!parent)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
